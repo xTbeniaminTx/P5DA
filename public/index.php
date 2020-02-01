bc6e28d11ec2e460230a96512eaa054e10221fd1
btolan-karudev <beniamin.tolan@outlook.com>
2020-02-01 21:52:26 +0100

    autoload custom helper session

--- public/index.php
+++ public/index.php
@@ -3,7 +3,7 @@
 
 use app\Router;
 
-require_once '../app/helpers/session_helper.php';
+//require_once '../app/helpers/session_helper.php';
 require_once('../app/libraries/Database.php');
 require_once '../vendor/autoload.php';
 require_once '../app/config/config.php';
