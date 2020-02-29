07e2538d2886a98e5f729b3fd3ff27e4e038ece7
btolan-karudev <beniamin.tolan@outlook.com>
2020-03-01 00:13:25 +0100

    mail refactor

--- app/services/Mail.php
+++ app/services/Mail.php
@@ -4,10 +4,6 @@
 namespace app\services;
 
 
-use Swift_Mailer;
-use Swift_Message;
-use Swift_SmtpTransport;
-
 class Mail
 {
     public static function send($to, $to_user_name, $user_email, $subject, $text, $html)
@@ -27,42 +23,6 @@ class Mail
             $response = $sendgrid->send($email);
             Session::addMessage('Mail envoyer avec succes');
             return Redirect::to('contact');
-        } catch (Exception $e) {
-            echo 'Caught exception: ' . $e->getMessage() . "\n";
-        }
-
-    }
-
-    public static function sendWithSwift()
-    {
-        if (isset($_POST['btnSubmit'])) {
-            //SWIFTMAILER
-            // Create the Transport
-            $transport = (new Swift_SmtpTransport('smtp.googlemail.com', 465, 'ssl'))
-                ->setUsername('beniamin777tolan@gmail.com')
-                ->setPassword('rewopi123456');
-
-            // Create the Mailer using your created Transport
-            $mailer = new Swift_Mailer($transport);
-
-            // Create a message
-            $message = (new Swift_Message('JF Blog Subject'))
-                ->setFrom(['noreply@jeanforteroche.me' => 'Blog JF'])
-                ->setReplyTo([$_POST['txtEmail'] => $_POST['txtName']])
-                ->setTo(['beniamin777tolan@gmail.com' => 'Admin JF'])
-                ->setBody('Message: ' . $_POST['txtMsg'])
-                ->addPart('<strong>Message:</strong><p> ' . $_POST['txtMsg'] . '</p><br/><strong>Telephone:</strong> ' . $_POST['txtPhone'], 'text/html');
-
-
-        }
-        try {
-            // Send the message
-            $result = $mailer->send($message);
-            header('Location: index.php?action=contact');
-            Session::addMessage('Message envoyee avec succès');
-            print $result->statusCode() . "\n";
-            print_r($result->headers());
-            print $result->body() . "\n";
         } catch (Exception $e) {
             echo 'Caught exception: ' . $e->getMessage() . "\n";
         }
